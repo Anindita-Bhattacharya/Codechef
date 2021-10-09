6013cf690651dc855ad89ec9decf272d0a8afef6
Anindita Bhattacharya <59307308+Anindita-Bhattacharya@users.noreply.github.com>
2021-10-09 13:16:06 +0530

    Create   Primality Test.py

---
+++ Primality Test.py
@@ -0,0 +1,14 @@
+t=int(input())
+while(t>0):
+    n=int(input())
+    if n>1:
+        
+        for i in range(2,n):
+            if n%i==0:
+                print("no")
+                break
+        else:
+            print("yes")
+    else:
+        print("no")
+    t=t-1
